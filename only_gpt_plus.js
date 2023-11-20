6703257e963e411a21eb91695eb95c7c25d451b1
lvguanjun <lvguanjun135@foxmail.com>
2023-11-21 02:46:00 +0800

    Refactor script and enhance button toggle functionality

--- only_gpt_plus.js
+++ only_gpt_plus.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Only Gpt Plus
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.1.0
 // @description  Toggle visibility of links without class "plus"
 // @author       lvguanjun
 // @match        https://chat-shared2.zhile.io/shared.html*
@@ -13,127 +13,92 @@
 (function () {
     'use strict';
 
-    // 初始隐藏不包含 "plus" 的元素
-    hideNonPlus();
-
-    // 在用户脚本中添加样式
-    let style = document.createElement('style');
+    // Add custom styles to the user script
+    const style = document.createElement('style');
     style.innerHTML = `
-    @keyframes shine-animation {
-        0% {
-            box-shadow: 0 0 3px 2px gold;
+        @keyframes shine-animation {
+            0% { box-shadow: 0 0 3px 2px gold; }
+            50% { box-shadow: 0 0 7px 6px gold; }
+            100% { box-shadow: 0 0 3px 1px gold; }
         }
-        50% {
-            box-shadow: 0 0 7px 6px gold;
-        }
-        100% {
-            box-shadow: 0 0 3px 1px gold;
-        }
-    }
 
-    @keyframes gradient-animation {
-        0% {
-            background-position: 0% 50%;
+        @keyframes gradient-animation {
+            0% { background-position: 0% 50%; }
+            50% { background-position: 100% 50%; }
+            100% { background-position: 30% 50%; }
         }
-        50% {
-            background-position: 100% 50%;
+
+        .gradient {
+            background-size: 200% 200%;
+            background-image: linear-gradient(45deg, #007BFF, #24C6DC, #FFD700, #24C6DC, #007BFF);
         }
-        100% {
-            background-position: 30% 50%;
+
+        .animate-gradient {
+            animation: gradient-animation 2s ease-in-out forwards;
         }
-    }
 
-    .gradient {
-        background-size: 200% 200%;
-        background-image: linear-gradient(45deg, #007BFF, #24C6DC, #FFD700, #24C6DC, #007BFF);
-    }
+        .gradient-shine {
+            animation: gradient-animation 2s ease-in-out forwards, shine-animation 3s ease-in-out infinite;
+        }
 
-    .animate-gradient {
-        animation: gradient-animation 2s ease-in-out forwards;
-    }
+        .flex-list li:not(:has(.plus)) {
+            display: none;
+        }
 
-    .gradient-shine {
-        animation: gradient-animation 2s ease-in-out forwards, shine-animation 3s ease-in-out infinite;
-    }
-`;
+        .flex-list.show-non-plus li:not(:has(.plus)) {
+            display: list-item;
+        }
+    `;
     document.head.appendChild(style);
 
-    // 创建一个按钮
-    let button = document.createElement('button');
+    // Create a button
+    const button = document.createElement('button');
     button.innerText = 'PLUS';
-    button.style.position = 'fixed';
-    button.style.top = '10px';
-    button.style.right = '10px';
-    button.style.zIndex = '1000';
-    button.style.padding = '15px 25px';
-    button.style.fontSize = '10px';
-    button.style.border = 'none';
-    button.style.borderRadius = '50px';
-    button.style.color = 'white';
-    button.style.cursor = 'pointer';
-    button.style.boxShadow = '0 5px 15px rgba(0,0,0,0.2)';
-    button.style.transition = 'all 0.3s ease';
+    button.style.cssText = `
+        position: fixed; top: 10px; right: 10px; z-index: 1000;
+        padding: 15px 25px; font-size: 10px; border: none;
+        border-radius: 50px; color: white; cursor: pointer;
+        box-shadow: 0 5px 15px rgba(0,0,0,0.2); transition: all 0.3s ease;
+    `;
+    button.classList.add('gradient', 'gradient-shine');
 
-    button.classList.add('gradient')
-    button.classList.add('gradient-shine');
+    // Mouse hover effects
+    button.onmouseover = () => {
+        button.style.cssText += 'transform: scale(1.05) translateY(-5px); box-shadow: 0 7px 20px rgba(0,0,0,0.3);';
+    };
 
-    // 鼠标悬停效果
-    button.onmouseover = function () {
-        button.style.transform = 'scale(1.05) translateY(-5px)';
-        button.style.boxShadow = '0 7px 20px rgba(0,0,0,0.3)';
-    }
+    button.onmouseout = () => {
+        button.style.cssText += 'transform: scale(1) translateY(0); box-shadow: 0 5px 15px rgba(0,0,0,0.2);';
+    };
 
-    // 鼠标移出效果
-    button.onmouseout = function () {
-        button.style.transform = 'scale(1) translateY(0)';
-        button.style.boxShadow = '0 5px 15px rgba(0,0,0,0.2)';
-    }
-
-    // 将按钮添加到页面上
+    // Append the button to the page
     document.body.appendChild(button);
 
-    button.onclick = toggleNonPlus;
+    // Function to toggle visibility of non-plus elements and change button style
+    button.onclick = () => {
+        const flexList = document.querySelector('.flex-list');
+        flexList.classList.toggle('show-non-plus');
 
-    function hideNonPlus() {
-        document.querySelectorAll('li > a:not(.plus)').forEach(e => {
-            e.parentNode.style.display = 'none';
-        });
-    }
-
-    function showNonPlus() {
-        document.querySelectorAll('li > a:not(.plus)').forEach(e => {
-            e.parentNode.style.display = '';
-        });
-    }
-
-    function toggleNonPlus() {
-        let firstNonPlus = document.querySelector('li > a:not(.plus)');
-        if (firstNonPlus && firstNonPlus.parentNode.style.display === 'none') {
-            showNonPlus();
+        if (flexList.classList.contains('show-non-plus')) {
             button.classList.remove('gradient-shine');
             setTimeout(() => {
                 button.classList.add('animate-gradient');
             }, 0);
         } else {
-            hideNonPlus();
             button.classList.remove('animate-gradient');
             setTimeout(() => {
                 button.classList.add('gradient-shine');
             }, 0);
         }
-    }
+    };
 
+    // Override Swal.fire function for specific calls
     const originalSwalFire = Swal.fire;
-
-    // 覆盖Swal.fire函数
     Swal.fire = function (settings) {
-        // 检查是否为特定的调用
         if (settings.title === '请珍惜账号' && settings.icon === 'warning') {
-            // 不要执行此Swal.fire调用
             console.log('Blocked Swal.fire with title: 请珍惜账号');
             return;
         }
-        // 否则，调用原始Swal.fire函数
         return originalSwalFire.apply(this, arguments);
     };
 })();
